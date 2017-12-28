bc73acbe1a239adbe8e5348f963f2d4f2f95d878
benice195 <szczyrbaw@gmail.com>
2017-12-28 19:52:38 +0100

    ToDo webpack

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Title = props => {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            <div>{props.number}</div>
+        </div>
+    );
+}
+export default Title;
